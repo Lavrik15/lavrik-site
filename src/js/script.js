e4dedbdde9620b351623757f8fdff871a91af016
Lavrik <lavrik.aw@ya.ru>
2016-10-21 13:07:47 +0300

    ajax

--- src/js/script.js
+++ src/js/script.js
@@ -1,24 +1,16 @@
-$(document).ready(function(){
-  $('a').on('click', clickLinklistener);
-});
-window.addEventListener("popstate", function(e) {
-  getContent(location.pathname, false);
-  console.log("1231231231231231231232132131212312312");
-});
-function getContent(url, addEntry) {
-  console.log(url);
-  $(".page").load(url + " #page-load", function(){
-    console.log('курлык');
-    if(addEntry == true) {
-       history.pushState(null, null, url); 
-    }
-  });
-}
-// html5 api history
-function clickLinklistener() {
-  console.log("клик");
-  var href = $(this).attr("href");
-  getContent(href, true);
+function clickLink(elem) {
+  event.preventDefault();
+  var href = '../pageload.json';
+
   console.log(href);
-  return false;
+  console.log(typeof href);
+  $.getJSON(href, 'limit=2', callBack);
+  // $(".page").load(href + " #page-load", function(){
+  //   console.log('курлык');
+  // });
+}
+function callBack(data) {
+  console.log(data)
+  console.log('kjkm');
+  // $('.page').html(dat);
 }
